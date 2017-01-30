3f44920bdc8236748d6a41562a7b3a5477d07974
22427 <->
2017-01-30 13:58:13 +0100

    added bounding box and operations

--- dev/vmath.h
+++ dev/vmath.h
@@ -24,6 +24,94 @@ typedef glm::mat2 mat2;
 typedef glm::mat3 mat3;
 typedef glm::mat4 mat4;
 
+class BoundingBox3
+{
+	vec3 m_min;
+	vec3 m_max;
+public:
+	BoundingBox3():m_min(NAN),m_max(NAN)
+	{
+	}
+
+	void add_point(const vec3& p)
+	{
+		if(std::isnan(m_min.x))
+			m_min = m_max = p;
+		else
+		{
+			for(uint32_t i= 0 ; i< 3;i++)
+			{
+				m_min[i] = m_min[i]<p[i]?m_min[i]:p[i];
+				m_max[i] = m_max[i]>p[i]?m_max[i]:p[i];
+			}
+		}
+	}
+
+	void add_point(const vec4& p)
+	{
+		add_point(vec3(p));
+	}
+
+	void add_point(const vec2& p)
+	{
+		add_point(vec3(p,0.0f));
+	}
+
+	vec3 closest_point(const vec3& p)
+	{
+		vec3 res;
+		for(uint32_t i = 0; i< 3;i++)
+			res[i] = (p[i]<m_min[i])? m_min[i]:((p[i]>m_max[i])? m_max[i]:p[i]);
+		return res;
+	}
+
+	vec3 closest_point(const vec3& p, bool& inside)
+	{
+		vec3 res;
+		inside = true;
+		for(uint32_t i = 0; i< 3;i++)
+		{
+			res[i] = (p[i]<m_min[i])? m_min[i]:((p[i]>m_max[i])? m_max[i]:p[i]);
+			inside &= p[i]>=m_min[i] && p[i]<=m_max[i];
+		}
+		return res;
+	}
+
+	bool inside(const vec3& p)
+	{
+		bool res = true;
+		for(uint32_t i = 0; i< 3;i++)
+			res &= p[i]>=m_min[i] && p[i]<=m_max[i];
+		return  res;
+	}
+
+	float dist_hull(const vec3& p)
+	{
+		return glm::distance(p,closest_point(p));
+	}
+
+	float dist_volume(const vec3& p)
+	{
+		return inside(p)?0.0f:glm::distance(p,closest_point(p));
+	}
+
+	float dist_volume2(const vec3& p)
+	{
+		return inside(p)?0.0f:glm::distance2(p,closest_point(p));
+	}
+
+	// negative for points on the outside
+	float signed_dist(const vec3& p)
+	{
+		bool in= true;
+		auto r = glm::distance(p,closest_point(p,in));
+		if(!in)
+			r*=-1.0f;
+		return r;
+	}
+
+
+};
 
 inline bool operator<(const vec2& a , const vec2& b)
 {
@@ -131,7 +219,7 @@ inline bool operator==(const vec4& a , const vec4& b)
 template<typename T>
 class Comperator
 {
-	public:
+public:
 	bool operator()(const T& a, const T&b)
 	{
 		return a <b;
